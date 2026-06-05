05a49c00b6d551520ff1cfd36dc636670c592fd2
Jaydee Bernardo <jdbernardo@Jaydees-Mac-mini.local>
2026-06-05 21:28:04 +0800

    feat(walker): emit sub-menu-link and menu-chevron classes

--- src/Walkers/HeaderNavWalker.php
+++ src/Walkers/HeaderNavWalker.php
@@ -12,12 +12,13 @@ class HeaderNavWalker extends Walker_Nav_Menu
         $has_children = in_array('menu-item-has-children', $classes);
         $is_active = untrailingslashit($item->url) === untrailingslashit(home_url(add_query_arg([])));
         $class_names = ($is_active ? 'text-gold' : 'text-white') . ' font-garet font-light text-base no-underline';
+        $class_names .= $depth > 0 ? ' sub-menu-link' : '';
 
         $output .= '<a href="' . esc_url($item->url) . '" class="' . $class_names . '">';
         $output .= esc_html($item->title);
 
         if ($has_children) {
-            $output .= '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="inline-block ml-1"><path d="m6 9 6 6 6-6"/></svg>';
+            $output .= '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="menu-chevron inline-block ml-1"><path d="m6 9 6 6 6-6"/></svg>';
         }
 
         $output .= '</a>';
